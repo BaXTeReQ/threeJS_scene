456b0423e1224f0386e8e810ff5255b670cc93d7
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2022-02-01 15:25:47 +0100

    Added comments, fixed code, deleted array

--- src/js/ground/street.js
+++ src/js/ground/street.js
@@ -1,41 +1,42 @@
 //STREET.JS
 
-const roadTexture1 = textureLoader.load('../ProjektGrafika/textures/road1.png');
-const roadTexture2 = textureLoader.load('../ProjektGrafika/textures/road2.png');
+/* textures */
+const roadTextures = [
+    textureLoader.load('../ProjektGrafika/textures/road1.png'),
+    textureLoader.load('../ProjektGrafika/textures/road2.png')
+]
 
-/* base street object */
+/* geometry */
 const streetGeometry = new THREE.BoxGeometry(1, 0.1, 1);
-const streetMaterial1 = new THREE.MeshStandardMaterial({ /* color: 0x34495e */color: 0xffffff, map: roadTexture1, side: THREE.DoubleSide});
-const streetMaterial2 = new THREE.MeshStandardMaterial({ /* color: 0x34495e */color: 0xffffff, map: roadTexture2, side: THREE.DoubleSide});
-const streetBase1 = new THREE.Mesh(streetGeometry, streetMaterial1);
-const streetBase2 = new THREE.Mesh(streetGeometry, streetMaterial2);
+
+/* materials */
+const streetMaterials = [
+    new THREE.MeshStandardMaterial({color: 0xffffff, map: roadTextures[0], side: THREE.DoubleSide}),
+    new THREE.MeshStandardMaterial({color: 0xffffff, map: roadTextures[1], side: THREE.DoubleSide})
+]
+
+/* objects */
+const streetBase1 = new THREE.Mesh(streetGeometry, streetMaterials[0]);
+const streetBase2 = new THREE.Mesh(streetGeometry, streetMaterials[1]);
+
+/* shadows */
 streetBase1.receiveShadow = true;
+streetBase2.receiveShadow = true;
 
+/* street objects */
+const street0 = streetBase1.clone()
+const street1 = streetBase2.clone()
 
-const streetsArray = new Array();
+/* positions */
+street0.position.set(0, 0, 17.5)
+street1.position.set(0, 0, 0)
 
-streetsArray[0] = streetBase1.clone();
-streetsArray[0].position.set(0, 0, 17.5);
-streetsArray[0].scale.set(10, 1, 25);
+/* scales */
+street0.scale.set(10, 1, 25)
+street1.scale.set(10, 1, 70)
 
-streetsArray[1] = streetBase2.clone();
-streetsArray[1].position.set(0, 0, 0);
-streetsArray[1].scale.set(10, 1, 70);
-streetsArray[1].rotateY(Math.PI/2)
+/* rotation */
+street1.rotateY(Math.PI / 2)
 
-
-
-scene.add(streetsArray[0], streetsArray[1])
-
-// streetsArray[0] = streetBase.clone();
-
-
-// for (let i = 0; i < 2; i++) {
-//     streetsArray[i] = streetBase.clone();
-//     streetsArray[i].scale.set(70, 1, 10)
-//     if (i == 1) {
-//         streetsArray[i].scale.set(10, 1, 25)
-//         streetsArray[i].position.set(0, 0, 17.5)
-//     }
-//     scene.add(streetsArray[i])
-// }
+/* adding to scene */
+scene.add(street0, street1)
