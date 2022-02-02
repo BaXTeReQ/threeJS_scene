8f0ba07c67244247e4d675a88e1de7d4f80e4d6e
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2022-02-02 16:01:51 +0100

    Added functions for different cameras

--- js/site.js
+++ js/site.js
@@ -648,6 +648,23 @@ house3.position.set(25, 2.5, 20);
 /* adding to scene */
 scene.add(house0, house1, house2, house3);
 "use strict";
+function camera1() {
+  camera.position.set(39, 4, 11);
+  controls.target.set(-25, 3, -23);
+}
+function camera2() {
+  camera.position.set(15, 4, -23);
+  controls.target.set(5, 3, -10);
+}
+function camera3() {
+  camera.position.set(-14, 3, 31);
+  controls.target.set(10, 3, -3);
+}
+function camera4() {
+  camera.position.set(-39, 3, 4);
+  controls.target.set(0, 3, 0);
+}
+"use strict";
 //RENDER.JS
 /* render scene */
 renderer.render(scene, camera);
---
+++ src/js/cameraSwitch.js
@@ -0,0 +1,19 @@
+function camera1(){
+    camera.position.set(39, 4, 11)
+    controls.target.set(-25, 3, -23)
+}
+
+function camera2(){
+    camera.position.set(15, 4, -23)
+    controls.target.set(5, 3, -10)
+}
+
+function camera3(){
+    camera.position.set(-14, 3, 31)
+    controls.target.set(10, 3, -3)
+}
+
+function camera4(){
+    camera.position.set(-39, 3, 4)
+    controls.target.set(0, 3, 0)
+}
