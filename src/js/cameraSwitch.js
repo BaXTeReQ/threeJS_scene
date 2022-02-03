0b923213addb89f09bd89330921b04d71d6b7460
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2022-02-03 12:04:00 +0100

    Added camera freeze on environment cameras, default camera is rotatable

--- src/js/cameraSwitch.js
+++ src/js/cameraSwitch.js
@@ -11,24 +11,30 @@ const btn5 = document.getElementById('btn5');
 btn1.addEventListener('click', ()=>{
     camera.position.set(39, 6, 11)
     controls.target.set(-25, 3, -23)
+    controls.enabled = false
 });
 
 btn2.addEventListener('click', ()=>{
     camera.position.set(25, 10, -26)
     controls.target.set(5, 3, -10)
+    controls.enabled = false
 });
 
 btn3.addEventListener('click', ()=>{
     camera.position.set(-14, 3, 31)
     controls.target.set(10, 3, -3)
+    controls.enabled = false
 });
 
 btn4.addEventListener('click', ()=>{
     camera.position.set(-39, 3, 4)
     controls.target.set(0, 3, 0)
+    controls.enabled = false
 });
 
 btn5.addEventListener('click', ()=>{
     camera.position.set(35, 35, -35)
     controls.target.set(0, 0, 0)
+    controls.enabled = true
+    controls.update()
 });
